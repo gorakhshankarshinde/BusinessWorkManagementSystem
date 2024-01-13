3ff2ebd21f4ec88ca8f4405ca64b050aed1355bb
Gorakh Shinde <gorakhshinde12@outlook.com>
2024-01-13 23:56:15 +0530

    AboutUs code added

---
+++ BusinessWorkManagementSystem/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessWorkManagementSystem.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult GetAboutUs()
+        {
+            return View();
+        }
+    }
+}
